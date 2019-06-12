b32e694c721f288e1f16f2f2b96b6e9717dea77b
demik <demik@atb>
2019-06-12 13:17:38 +0200

    added debian build directory

---
+++ debian/check-ice2.default.ex
@@ -0,0 +1,10 @@
+# Defaults for check-ice2 initscript
+# sourced by /etc/init.d/check-ice2
+# installed at /etc/default/check-ice2 by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ tools/debian.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+dh_make --native --single --copyright bsd --packagename check-ice2_1.0.5 --email demik475@gmail.com
+
