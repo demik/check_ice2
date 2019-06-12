1ec3c1188a30abe3b96e4e5ac9e7167720b15982
demik <demik475@gmail.com>
2019-06-12 13:37:11 +0200

    cleaned debian dir

--- debian/check-ice2.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the check-ice2 package
-#
-0 4	* * *	root	[ -x /usr/bin/check-ice2_maintenance ] && /usr/bin/check-ice2_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(check-ice2):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="check-ice2" command="/usr/bin/check-ice2"
